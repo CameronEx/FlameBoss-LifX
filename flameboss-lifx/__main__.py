5b575b224d2ddff3f0e8d02f1ad6c189bc0c1152
Cameron Exley <github@exley.com.au>
2019-04-06 11:22:58 +1100

    Linting, debugging __main__.py

--- flameboss-lifx/__main__.py
+++ flameboss-lifx/__main__.py
@@ -1,8 +1,10 @@
 from flameboss_monitor import FlameBossMon
 from lifx_controller import LifxController
 from configparser import ConfigParser
+import logging
 import sys
 import os
+
 
 class FBLifX:
     """
@@ -11,15 +13,16 @@ class FBLifX:
     """
 
     def __init__(self):
-        fb = flameboss_monitor()
+        fb = FlameBossMon()
         lc = LifxController()
         self.config = ConfigParser()
         self.config_dir: str = os.path.expanduser("~/.config/FlameBoss-LifX/")
-        self.config_file: str = config_dir + "config.ini"
+        self.config_file: str = self.config_dir + "config.ini"
         self.controller_id: str = None
-        elf.poll_interval: srt = None
+        self.poll_interval: str = None
         self.bulb_mac: str = None
         self.bulb_ip: str = None
+        self.bulb_label: str = None
 
     def load_config(self):
         """
@@ -27,35 +30,35 @@ class FBLifX:
         """
 
         if not os.path.exists(self.config_dir):
-            logger.DEBUG(f"load_config: {self.config_dir} doesn't exist. Calling self.intial_config")
-            self.intial_config()
+            logger.debug(f"load_config: {self.config_dir} doesn't exist. Calling self.intial_config")
+            self.initial_config()
         else:
-            logger.DEBUG(f"Config directory {self.config_dir} already exists.")
+            logger.debug(f"Config directory {self.config_dir} already exists.")
 
-        logging.DEBUG("Attempting to read config file.")
-        if not config.read(self.config_file):
+        logging.debug("Attempting to read config file.")
+        if not self.config.read(self.config_file):
             logging.WARNING("Configuration file doesn't exist or is malformed.")
             self.initial_config()
         else:
-            self.controller_id = config["LAST_USED"]["controller_id"]
-            self.poll_interval = config["LAST_USED"]["poll_interval"]
-            self.bulb_mac = config["LAST_USED"]["bulb_mac"]
-            self.bulb_ip = config["LAST_USED"][["bulb_ip"]
-            bulb_label = config["LAST_USED"]["bulb_label"]
+            self.controller_id = self.config["LAST_USED"]["controller_id"]
+            self.poll_interval = self.config["LAST_USED"]["poll_interval"]
+            self.bulb_mac = self.config["LAST_USED"]["bulb_mac"]
+            self.bulb_ip = self.config["LAST_USED"]["bulb_ip"]
+            self.bulb_label = self.config["LAST_USED"]["bulb_label"]
 
             print(f"Last saved configuration:\n"
-                  "FlameBoss Controller: {self.controller_id}\n"
-                  "LifX Bulb Name: {bulb_label}\n"
+                  f"FlameBoss Controller: {self.controller_id}\n"
+                  f"LifX Bulb Name: {bulb_label}\n"
                   "Note: if the IP address of the bulb has changed, we will need to perform discovery again."
                   )
             valid_answer: bool = False
             count: int = 0
             while not valid_answer:
                 use_existing_config = input("Would you like to use the above configuration? [Y/N]")
-                if use_existing_config.to_lower() = "y":
+                if use_existing_config.lower() == "y":
                     # load config
                     valid_answer = True
-                elif use_existing_config = "n":
+                elif use_existing_config == "n":
                     self.initial_config()
                     valid_answer = True
                 else:
@@ -66,31 +69,30 @@ class FBLifX:
                     else:
                         print("Invalid answer, try again.")
 
-
-    def intial_config(self):
+    def initial_config(self):
         """
         Set up, or overwrite configuration file.
         TODO: test me under different scenarios (will this overwrite existing config?)
         """
         # Discover bulbs available on the local LAN
         logging.info("Starting Setup.\nDiscovering bulbs...")
-        lc.discover_bulbs()
+        self.lc.discover_bulbs()
         self.lc.select_target()
         bulb_config = lc.save_config()
-        logging.DEBUG(f"Selected bulb MAC: {bulb_mac}, IP: {bulb_ip}")
+        logging.debug(f"Selected bulb MAC: {self.bulb_mac}, IP: {self.bulb_ip}")
 
         # Obtain the controller ID.
         logging.INFO("Enter the ID of your Flame Boss controller.")
         logging.INFO("This is displayed on the LCD of your controller (if you have one)")
         logging.INFO("or can be found at https://myflameboss.com/en/users/edit")
-        controller_id = input("Enter the ID of your FlameBoss controller: ")
+        controller_id = input("Controller ID: ")
 
         # Create the directory
-        os.makedirs(config_dir)
-        logging.DEBUG(f"intial_config: Created {self.config_dir}")
+        os.makedirs(self.config_dir)
+        logging.debug(f"intial_config: Created {self.config_dir}")
 
         # Create the configuration to be written
-        setup_config = {
+        setup_config: dict = {
                          "controller_id": controller_id,
                          "poll_interval": 300,
                          "bulb_mac": bulb_config["mac_addr"],
@@ -98,11 +100,11 @@ class FBLifX:
                          "bulb_label": bulb_config["label"]
                         }
         # Write the config
-        logging.DEBUG(f"Saving config:\n{setup_config}")
-        config['LAST_USED'] = setup_config
-        with open(config_file, "w") as file:
-            config.write(file)
-            logging.DEBUG("initial_config: Wrote config successfully.")
+        logging.debug(f"Saving config:\n{setup_config}")
+        self.config['LAST_USED'] = setup_config
+        with open(self.config_file, "w") as file:
+            self.config.write(file)
+            logging.debug("initial_config: Wrote config successfully.")
 
     def run(self):
         """
@@ -110,4 +112,15 @@ class FBLifX:
         """
         # Load the configuration, if no config exists this module will call the initial setup
         self.load_config()
-        
+
+
+if __name__ == "__main__":
+    # Logger config
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.debug)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.debug)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
